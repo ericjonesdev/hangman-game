e5422b4ad5e8d7e008d26a09234360f260f279cc
ericjonesdev <>
2023-09-30 18:10:43 +0000

    bare bones of game before additional functionality

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from hangman_art import stages, logo
+from hangman_words import word_list
+import random
+import os
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -16,7 +20,72 @@ gamers = SHEET.worksheet('gamers')
 hilltop = SHEET.worksheet('hilltop')
 
 data = gamers.get_all_values()
+
 data2 = hilltop.get_all_values()
 
-print(data)
-print(data2)
+# Opening game logo
+print(logo)
+
+end_of_game = False
+word_list = word_list
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+
+# define a function to clear the user screen
+def clear():
+    os.system('clear')
+
+# Create a variable called 'lives' to keep track of the number of lives left. 
+# Set 'lives' to equal 6.
+
+lives = 6
+# Create blanks to illustrate blank word choice
+display = []
+for _ in range(word_length):
+    display += "_"
+
+
+you_chose = []
+# loop to keep game going while condition 'end_of_game' is equal to False
+while not end_of_game:
+    print(' '.join(display))  # Display the current status of the word
+
+    guess = input("Guess a letter: ").lower()
+    clear()
+
+    if guess in you_chose:
+        print(f"You chose {guess}. You already guessed that.")
+    else:
+        you_chose.append(guess)
+
+    if guess not in chosen_word:
+        print(f"You chose {guess}. That's not in the word. You lose a life!")
+
+    # Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        
+        if letter == guess:
+            display[position] = letter
+
+    # If guess is not a letter in the chosen_word, 
+    # then reduce the amount of lives by 1.
+    # When lives go down to 0, game is over and "You lose" is printed
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You Lose!!")
+
+
+    # Check if user has got all letters
+    if "_" not in display:
+        end_of_game = True
+        print("You Win!!")
+
+    # Print the ASCII art from 'stages' that corresponds to the current number of 'lives' 
+    # the user has remaining
+    print(stages[lives])
+
+    
